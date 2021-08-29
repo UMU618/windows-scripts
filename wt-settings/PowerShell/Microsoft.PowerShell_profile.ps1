6b86c76d03f0fe8d920265d03e92921974df4c00
UMU618 <umu618@hotmail.com>
2021-08-30 01:05:29 +0800

    + UMU-Light

--- wt-settings/PowerShell/Microsoft.PowerShell_profile.ps1
+++ wt-settings/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,14 @@
+Set-PSReadLineKeyHandler -Key "Ctrl+q" -Function MenuComplete
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module Get-ChildItemColor
-Set-Theme UMU
+Set-Theme UMU-Light
+
+function ll { Get-ChildItem -Force $args }
+
+function gcam { & git commit -am $args }
+function gpl { & git pull $args }
+function gph { & git push $args }
+function grv { & git remote -v }
+function gs { & git status }
