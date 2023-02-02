fcfbdd02dbe01f771ba50cf65afdff3d0dd38be0
UMU <umu618@hotmail.com>
2023-02-03 00:12:25 +0800

    + SessionID & PID & CreationDate

--- jscript/monitor_process.js
+++ jscript/monitor_process.js
@@ -3,35 +3,37 @@
 // monitor_process.js
 
 var strCScript = WScript.Path + "\\cscript.exe";
-if (WScript.FullName != strCScript)
-{
-        new ActiveXObject("WScript.Shell").Run(strCScript + " \"" + WScript.ScriptFullName + "\" //Nologo");
-        WScript.Quit(-1);
+if (WScript.FullName != strCScript) {
+	new ActiveXObject("WScript.Shell").Run(strCScript + " \"" + WScript.ScriptFullName + "\" //Nologo");
+	WScript.Quit(-1);
 }
 
 var objWMI = GetObject("winmgmts:{impersonationLevel=impersonate}!\\\\.\\root\\cimv2");
-var ps = objWMI.ExecNotificationQuery( "select * from __instancecreationevent within 1 where TargetInstance isa 'Win32_Process'" );
+var ps = objWMI.ExecNotificationQuery("select * from __instancecreationevent within 1 where TargetInstance isa 'Win32_Process'");
+
+var swdt = new ActiveXObject("WbemScripting.SWbemDateTime");
 
 WScript.Echo("monitor_process.js by UMU @ 2009-03-16");
 WScript.Echo("Press Ctrl+C to quit");
 WScript.Echo("监视进程创建鹰……");
 WScript.Echo("");
-for (;;)
-{
-        var p = ps.NextEvent();
+for (; ;) {
+	var p = ps.NextEvent();
 
-        try
-        {
-                WScript.StdOut.WriteLine(p.TargetInstance.Name);
-                WScript.StdOut.WriteLine(p.TargetInstance.ExecutablePath);
-                WScript.StdOut.WriteLine(p.TargetInstance.CommandLine);
-        }
-        catch (ex)
-        {
-                WScript.StdOut.WriteLine(ex);
-        }
-        WScript.StdOut.WriteLine("");
+	try {
+		swdt.Value = p.TargetInstance.CreationDate;
+		WScript.StdOut.WriteLine(swdt.GetVarDate());
+		WScript.StdOut.WriteLine('$' + p.TargetInstance.SessionId + ' [' + p.TargetInstance.ProcessId + '] ' + p.TargetInstance.Name);
+		// WScript.StdOut.WriteLine('[' + swdt.GetVarDate() + '] $' + p.TargetInstance.SessionId + ' [' + p.TargetInstance.ProcessId + '] ' + p.TargetInstance.Name);
+		WScript.StdOut.WriteLine(p.TargetInstance.ExecutablePath);
+		WScript.StdOut.WriteLine(p.TargetInstance.CommandLine);
+	}
+	catch (ex) {
+		WScript.StdOut.WriteLine(ex);
+	}
+	WScript.StdOut.WriteLine("");
 }
 
 delete ps;
 delete objWMI;
+delete swdt;
