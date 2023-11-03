89651a296d31d61266fd2cc91a37330ebac8425d
UMU <UMU618@users.noreply.github.com>
2023-11-03 22:04:09 +0800

    * Improve get win event

--- pwsh/list-logon-failed-ips.ps1
+++ pwsh/list-logon-failed-ips.ps1
@@ -1,15 +1,22 @@
 $ips = @{}
 Get-WinEvent -LogName Security -FilterXPath '*[System[band(Keywords,4503599627370496)] and EventData[Data[@Name="LogonType"]=3]]' | %{
     $xml = [xml]$_.toXml()
+    $user = ''
+    $ip = ''
     foreach ($data in $xml.Event.EventData.Data) {
-        if ($data.Name -eq 'IpAddress') {
+        if ($data.Name -eq 'TargetUserName') {
+            $user = $data.'#text'
+        }
+        elseif ($data.Name -eq 'IpAddress') {
             $ip = $data.'#text'
-            if ($ips.ContainsKey($ip)) {
-                ++$ips[$ip]
-            } else {
-                $ips.Add($ip, 1)
-                'Fisrt find ' + $ip + ' on ' + $_.TimeCreated
-            }
+        }
+    }
+    if (-not $ip.Equals('')) {
+        if ($ips.ContainsKey($ip)) {
+            ++$ips[$ip]
+        } else {
+            $ips.Add($ip, 1)
+            'Fisrt find ' + $ip + ' with ' + $user + ' on ' + $_.TimeCreated
         }
     }
 }
